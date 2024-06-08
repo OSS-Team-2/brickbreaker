13dee8ba072fe80022236c912d217a437841ebb4
Probius-ai <kcfm0529@naver.com>
2024-06-08 22:05:32 +0900

    add function to change color and be destroyed when the brick color is red

--- lib/src/objects/brick.dart
+++ lib/src/objects/brick.dart
@@ -1,6 +1,7 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 import '../brickbreakergame.dart';
 import '../config.dart';
@@ -9,11 +10,13 @@ import 'bat.dart';
 
 class Brick extends RectangleComponent 
   with HasGameRef<BrickBreaker>, CollisionCallbacks{
+    
+  int colorIndex;
   // Add your custom properties and methods here
   Brick({
     required super.position,
     required Color color,
-  }) : super(
+  }) : colorIndex = Random().nextInt(brickColors.length), super(
     size: Vector2(brickWidth, brickHeight),
     anchor: Anchor.center,
     paint: Paint()
@@ -22,10 +25,22 @@ class Brick extends RectangleComponent
     children: [RectangleHitbox()],
     );
 
+  // Method to handle brick hit
+  void hit() {
+    if (colorIndex < brickColors.length - 1) {
+      colorIndex++;
+      paint.color = brickColors[colorIndex];
+    } else {
+      // Brick is red and will be removed
+      removeFromParent();
+    }
+  }
+
   @override
   void onCollisionStart(
     Set<Vector2> intersectionPoints, PositionComponent other) {
     // Implement the collision logic for the brick
+    hit();
     super.onCollisionStart(intersectionPoints, other);
     removeFromParent();
     if (game.streak.value == 0) { // when streak is 0 add 1 to score
